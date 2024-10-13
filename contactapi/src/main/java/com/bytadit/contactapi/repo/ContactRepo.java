1e583e0d6bb01e54c47a764d39285658b9eddfdf
bytadit <adityabagusp345@gmail.com>
2024-10-13 21:42:56 +0700

    Create Contact Domain, Service, Repo

---
+++ contactapi/src/main/java/com/bytadit/contactapi/repo/ContactRepo.java
@@ -0,0 +1,13 @@
+package com.bytadit.contactapi.repo;
+
+import com.bytadit.contactapi.domain.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ContactRepo extends JpaRepository<Contact, String> {
+    Optional<Contact> findById(String id);
+
+}
